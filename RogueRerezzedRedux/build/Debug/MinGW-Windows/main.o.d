b18752b8cad7b9c90c38bef3ff349511d048072f
Mnenmenth <iamrazim12@gmail.com>
2015-02-18 07:53:38 -0500

    Stuff

---
+++ RogueRerezzedRedux/build/Debug/MinGW-Windows/main.o.d
@@ -0,0 +1,3 @@
+build/Debug/MinGW-Windows/main.o: main.cpp c:/toolkits/winapifamily.h
+
+c:/toolkits/winapifamily.h:
---
+++ RogueRerezzedRedux/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Razim Zavac
+ *
+ * Created on February 17, 2015, 6:20 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <SDL.h>
+using namespace std;
+
+int main(int argc, char* args[]) {
+    SDL_Window** window;
+    SDL_Renderer** renderer;
+    SDL_Surface* screen = NULL;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_OPENGL, window, renderer);
+    return 0;
+}
+
