062190f2dce98197b9202298930fc8dd701f265f
Mnenmenth <iamrazim12@gmail.com>
2015-02-20 08:45:20 -0500

    Torri I maybe fixed your things

--- RogueRerezzedRedux/Item.cpp
+++ RogueRerezzedRedux/Item.cpp
@@ -7,16 +7,58 @@
 
 #include "Item.h"
 #include <string>
+#include <stdio.h>
+#include <map>
+#include <iostream>
 
 using namespace std; 
 
-Item::Item(int id) {
-    this->id = id;
+/*
+ *  READ THIS TORRI
+ *  OK SO MAYBE YOU DON'T LIKE ITS NOT STATIC AND CONST AND CRAP
+ *  DEAL WITH IT FOR NOW BECUASE IT WORKS LIKE THIS
+ */
+
+int Item::ItemThing(int id){
+    const static string keys[] = {
+        "weapon", "armor",
+        "other", "material",
+        "modifier"
+    };
+    map<string, string> item;
+    item["weapon"] = 
+        "Sword", "Axe",
+        "Bow", "Crossbow",
+        "Wand", "Staff"
+    ;
+    item["armor"] = 
+        "Helm", "Chestpiece",
+        "Greaves", "Boots",
+        "Ring"
+    ;
+    item["other"] = 
+        "Scroll", "Tome",
+        "Potion", "Gold",
+        "Food", "Note",
+        "Crystal Shard"
+    ;
+    item["material"] = 
+        "Leather", "Chainmail",
+        "Iron", "Steel",
+        "Hyperium", "Alanium",
+        "Tsinium", "Visium"
+    ;
+    item["modifier"] = 
+        "Cursed", "Broken", "Weak",
+        "Good", "Great", "Legendary",
+        "Powerful", "Magical", "Ancient"
+    ;
+    id = id;
     string name = "";
     int mod = id % item["modifier"].size();
     int mat = id % item["material"].size();
     int type = id % (item["weapon"].size() + item["armor"].size() + item["other"].size());
-    name << item["modifier"][mod];
-    name << " " << item["material"][mat];
-    
+    name += item["modifier"][mod];
+    name += " "; 
+    name += item["material"][mat];
 }
