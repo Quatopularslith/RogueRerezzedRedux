e9d2c1ea771475d0cd7da31c4951cf9cb912b085
Mnenmenth <iamrazim12@gmail.com>
2015-02-25 17:19:31 -0500

    MButton workings

--- RogueRerezzedReduxScala/src/main/scala/input/MButton.scala
+++ RogueRerezzedReduxScala/src/main/scala/input/MButton.scala
@@ -1,8 +1,44 @@
 package input
 
+import java.awt.geom.AffineTransform
+import java.awt.image.{AffineTransformOp, BufferedImage}
+
+import graphics.ImageCache
+
 import scala.swing.Panel
 
 
-class MButton(pos: (Int, Int), img: String) extends Panel{
+object MButton extends Panel {
 
+}
+
+trait Button {
+  protected var pos: (Int, Int)
+  protected var scale: Double
+
+  def position = (Int, Int) = pos
+
+  def image: BufferedImage
+
+  def rescale(sc: Double) = scale = sc
+}
+
+class GenericButton(var pos: (Int, Int), var img: String, var scale: Double = 1.0, var action: Unit) extends Button {
+  var image_ = image
+
+  private def loadImage = {
+    val image = ImageCache.loadImage(img)
+    val transform = new AffineTransformOp(AffineTransform.getScaleInstance(scale, scale), AffineTransformOp.TYPE_BICUBIC)
+    val result = transform.createCompatibleDestImage(image, null)
+    transform.filter(image, result)
+  }
+
+  private def reload = image_ = loadImage
+
+  def image = image_
+
+  override def rescale(sc: Double) = {
+    super.rescale(sc)
+    reload
+  }
 }
