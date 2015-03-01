7dbf5a20a2008f4731266dfb6795636fdd8d92c7
Creatorri <torridon@live.com>
2015-03-01 16:21:35 -0500

    DungeonGen

--- RogueRerezzedReduxScala/src/main/scala/generators/Dungeon.scala
+++ RogueRerezzedReduxScala/src/main/scala/generators/Dungeon.scala
@@ -9,8 +9,8 @@ import scala.util.Random
  */
 object Dungeon {
   val spawnRoomSize = 5
+  val maxSize = 10
   val edges = ArrayBuffer.empty[(Int, Int)]
-  val chance = Array(0.6,0.4) //[0]=Hallway, [1]=Square
   private val rand = new Random()
 
   def fits(shape: Shape, floor: Map[(Int, Int), Tile]) = shape.footprint.forall(t => !floor.contains(t))
@@ -24,16 +24,34 @@ object Dungeon {
 
   def getEdges(floor: Map[(Int, Int), Tile]) = floor.filterKeys(t => (for (dx <- List(-1, 1); dy <- List(-1, 1)) yield floor.contains((t._1 + dx, t._2 + dy))).contains(false))
 
+  def chooseShape(pos: (Int, Int)): Shape = {
+    val check = rand.nextDouble()
+    if(check < 0.4){
+      return new Rect(pos, (rand.nextInt() % maxSize, rand.nextInt() % maxSize))
+    }else{
+      return new Hallway(pos, rand.nextInt() % maxSize)
+    }
+  }
+
   def genDungeon(size: (Int, Int)):Map[(Int, Int), Tile] = {
     val floor = Map.empty[(Int, Int), Tile]
     addShape(new Square((size._1 / 2,size._2 / 2), spawnRoomSize), floor)
     var stop = false
     var noSpace = 0
+    var chosen = rand.shuffle(getEdges(floor).keys).head
+    var shape = chooseShape((0,0))
     while(!stop){
-      val chosen = rand.shuffle(getEdges(floor).keys).head
-      
+      chosen = rand.shuffle(getEdges(floor).keys).head
+      shape = chooseShape(chosen)
+      if(fits(shape, floor)){
+        addShape(shape, floor)
+      }else{
+        noSpace += 1
+      }
+      if(noSpace > 10){
+        stop = true
+      }
     }
-
-    return null
+    return floor
   }
 }
